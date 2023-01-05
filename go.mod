d5728d6c547756a87f6e35839dc7b8e997571dc5
geekr <geekr@hey.com>
2023-01-06 00:21:27 +0800

    基于cmux实现grpc/http请求处理

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/soheilhy/cmux v0.1.5
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
--- main.go
+++ main.go
@@ -9,61 +9,63 @@ import (
 	pb "github.com/geekr-dev/go-tag-service/proto"
 	"github.com/geekr-dev/go-tag-service/server"
 
+	"github.com/soheilhy/cmux"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-var grpcPort string
-var httpPort string
+var port string
 
 func init() {
-	flag.StringVar(&grpcPort, "grpc_port", "9000", "gRPC 启动端口")
-	flag.StringVar(&httpPort, "http_port", "8001", "HTTP 启动端口")
+	flag.StringVar(&port, "port", "9000", "启动端口号")
 	flag.Parse()
 }
 
 func main() {
+	// 先监听 TCP 端口，gRPC 和 HTTP 都是基于 TCP 的
+	lis, err := startTcpServer(port)
+	if err != nil {
+		log.Fatalf("start tcp server failed: %v", err)
+	}
 
-	errs := make(chan error)
-	go func() {
-		err := startHttpServer(httpPort)
-		if err != nil {
-			errs <- err
-		}
-	}()
+	m := cmux.New(lis)
+	// gRPC 基于 application/grpc 请求头进行分流
+	grpcLis := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldPrefixSendSettings("content-type", "application/grpc"))
+	httpLis := m.Match(cmux.HTTP1Fast())
 
-	go func() {
-		err := startGrpcServer(grpcPort)
-		if err != nil {
-			errs <- err
-		}
-	}()
+	grpcServer := initGrpcServer()
+	httpServer := initHttpServer(port)
+	go grpcServer.Serve(grpcLis)
+	go httpServer.Serve(httpLis)
 
-	err := <-errs
+	err = m.Serve()
 	if err != nil {
 		log.Fatalf("start server failed: %v", err)
 	}
 }
 
-// 启动 HTTP 服务器
-func startHttpServer(port string) error {
-	serverMux := http.NewServeMux()
-	serverMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte(`pong`))
-	})
-	return http.ListenAndServe(":"+port, serverMux)
+// TCP 服务器
+func startTcpServer(port string) (net.Listener, error) {
+	return net.Listen("tcp", ":"+port)
 }
 
-// 启动 gRPC 服务器
-func startGrpcServer(port string) error {
+// gRPC 服务器
+func initGrpcServer() *grpc.Server {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
 
-	lis, err := net.Listen("tcp", ":"+port)
-	if err != nil {
-		return err
-	}
+	return s
+}
 
-	return s.Serve(lis)
+// HTTP 服务器
+func initHttpServer(port string) *http.Server {
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`pong`))
+	})
+	return &http.Server{
+		Addr:    ":" + port,
+		Handler: serverMux,
+	}
 }
